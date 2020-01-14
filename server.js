8e0831524d56cb1da8fed3b2a10f5c3423d84a6d
caitleigh <caitleigh27@gmail.com>
2020-01-14 12:29:28 -0800

    added location route and half compelted feature 1

--- server.js
+++ server.js
@@ -3,9 +3,30 @@
 // require the libraries //
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//the policeperson of our server - allows our server to talk to the frontend//
+app.use(cors());
+
+//Routes//
+//LOCATIONS//
+app.get('/location', (request, response) => {
+  let city = request.query.city;
+  const geoData = require('./data/geo.json');
+
+  let location = {
+    search_query: city,
+    formatted_query: geoData[0].display_name,
+    latitude: geoData[0].lat,
+    longitude: geoData[0].lon
+  }
+  response.status(200).send(location);
+})
+
+
 
 //turn it on//
 app.listen(PORT, () => {
