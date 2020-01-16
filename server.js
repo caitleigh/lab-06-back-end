4c8aa6a347d49176dd99c6b0e644e610f9c72ef9
caitleigh <caitleigh27@gmail.com>
2020-01-16 13:36:13 -0800

    set up database, added pg, , updated read me

--- server.js
+++ server.js
@@ -4,10 +4,13 @@
 const express = require('express');
 require('dotenv').config();
 const superagent = require('superagent');
-const cors = require('cors')
+const cors = require('cors');
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', err => console.error(err));
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 //the policeperson of our server - allows our server to talk to the frontend//
 app.use(cors());
@@ -20,6 +23,25 @@ app.use(cors());
 // app.use(errorHandler);
 // app.use('*', noResponseError);
 
+////SQL/////
+// app.get('/add', (request, response)=> {
+//   let search_query = request.query.search_query;
+//   let formatted_query = request.query.formatted_query;
+//   let latitude = request.query.latitude;
+//   let longitude = request.query.longitude;
+//   console.log('search_query: ', search_qry);
+//   console.log('formatted query: ', formatted_query);
+//   console.log('latitude: ', latitude);
+//   console.log('longitude: ', longitude);
+
+//   let sql = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ( $1, $2, $3, $4);';
+//   let safeValues = [search_query, formatted_query, latitude, longitude];
+//   client.query(sql, safeValues)
+//     .then(results => {
+//       response.status(200).json(results)
+//     })
+//     .catch(error => console.error('error ', error));
+// });
 
 //////LOCATION///////
 app.get('/location', (request, response) => {
@@ -32,7 +54,10 @@ app.get('/location', (request, response) => {
       .then( data => {
         const geoDataResults = data.body[0];
         const location = new Location(city, geoDataResults)
-        // locations[url] = location;
+
+        let sql = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ( $1, $2, $3, $4);';
+        let safeValues = [location.search_query, location.formatted_query, location.latitude, location.longitude];
+        client.query(sql, safeValues).catch(error => console.error('error ', error));
         response.status(200).send(location);
       })
   }
@@ -77,42 +102,51 @@ function DailySummaries(day) {
 
 ////////EVENTS/////////////
 
-// http://api.eventful.com/rest/events/search?...&where=${location.latitude},${location.longitude}&within=5`
 
-// https://eventful.com/oauth/authoriz
 // app.get('/events', (request, response) => {
 //   try {
-//     let latitude = request.query.latitude;
-//     let longitude = request.query.longitude;
+//     let search_query = request.query.search_qeury;
+//     console.log('search q: ', search_query);
+//     let key = process.env.EVENTFUL_API_KEY;
+//     const url = `http://api.eventful.com/json/events/search?location=${search_query}&app_key=${key}`;
+//     console.log('url:', url);
+//     superagent.get(url)
+//       .then(data => {
+//         let parse = JSON.parse(data.text);
+//         let events = parse.events;
+//         let eventInfo = events.map (val => {
+//           return new Event(val);
+//         });
+//         response.status(200).send(eventInfo);
+//       })}catch(error){
+//     errorHandler('So sorry, something went wrong', request, response);
 //   }
+// });
 
-
-// })
-
-// {
-//   "link": "http://seattle.eventful.com/events/geekzonehosting-raspberry-pi-jam-session-code-c-/E0-001-121109275-3?utm_source=apis&utm_medium=apim&utm_campaign=apic",
-//   "name": "GeekZoneHosting Raspberry Pi Jam Session & Code Carnival 2019",
-//   "event_date": "Sat Dec 7 2019",
-//   "summary": "Join fellow coders, builders, and Raspberry Pi makers in an 8 hour all day event Jam Session builder and code-a-thone to celebrate computer science education week 2019."
-// },
-
-// function Event(city, ){
-//   this.link = link;
-//   this.name =
-//   this.event_date =
-//   this.summary =
+// /// Constructor function for events///
+// function Event(search_qeury, eventData){
+//   this.link = eventData.url;
+//   this.name = eventData.title;
+//   this.event_date = eventData.start_time;
+//   this.summary = eventData.description;
 // }
+
+
 
 //////////////////////////////////////////////
 function errorHandler(string, response){
   response.status(500).send(string)
 }
 
-function noResponseError(request, response){
-  response.status(404).send('huh?');
-}
+// function noResponseError(request, response){
+//   response.status(404).send('huh?');
+// }
 
 //turn it on//
-app.listen(PORT, () => {
-  console.log(`listen on ${PORT}`);
-});
+client.connect()
+  .then(app.listen(PORT, () => console.log(`listen on ${PORT}`)))
+  .catch((err) => console.error(err));
+
+// app.listen(PORT, () => {
+//   console.log(`listen on ${PORT}`);
+// });
